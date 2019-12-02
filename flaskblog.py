7a9c46aa257dc0965cafcdce421dc4a8a2092d0e
Ruben <rubenpm88@gmail.com>
2019-12-02 13:48:31 +0100

    add 2 simple routes

---
+++ flaskblog.py
@@ -0,0 +1,17 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    name = request.args.get("name", "Page")
+    return f'<h1>Home {escape(name)}!</h1>'
+
+@app.route('/about')
+def about():
+    name = request.args.get("name", "Page")
+    return f'<h1>About {escape(name)}!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
